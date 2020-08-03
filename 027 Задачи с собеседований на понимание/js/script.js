57bb45081840a568f2b952e5361e9dfaaa5f9555
Eissberg <jeka.chk@gmail.com>
2020-08-03 23:01:17 +0300

    27. Задачи с собеседований на понимание основ

---
+++ 027 Задачи с собеседований на понимание/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// let x = 5; alert( x++ );
+
+// [] + false - null + true
+//console.log([] + false - null + true); // "NaN"
+
+//let y = 1;
+//let x = y = 2;
+//alert(x);
+
+//console.log([] + 1 + 2); //"12"
+
+//alert( "1"[0] );
+
+//console.log(2 && 1 && null && 0 && undefined);
+// И всегда запинается на лжи и дальше не работает
+
+// console.log(!! (1 && 2) === (1 && 2));
+
+// alert( null || 2 && 3 || 4 ); //3
+//ИЛИ всегда запинается на правде
+
+//let a = [1, 2, 3], b = [1, 2, 3]; // не буду равны
+
+//alert( +"Infinity"); // Infinity как числовое представление
+
+//console.log("Ёжик" > "яблоко"); // false
+
+//console.log(0 || "" || 2 || undefined || true || false); // 2
