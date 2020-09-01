a9f9eb20d784bcab23b124f2f464a94c8c2494a4
Eissberg <jeka.chk@gmail.com>
2020-09-01 23:38:29 +0300

    38. Создаем табы в новом проекте

--- 038 Создаем табы в новом проекте/js/script.js
+++ 038 Создаем табы в новом проекте/js/script.js
@@ -1,68 +1,42 @@
 'use strict';
 
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector('.btn-block');
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
 
-//console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(0));
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            // item.style.display = 'none';
+            item.classList.add('hide');        //переделываем через классы
+            item.classList.remove('show', 'fade');     //переделываем через классы
+        });
 
-// console.log(btns[0].classList.add('red', 'tratata'));      //Добавление класса
-// console.log(btns[0].classList.remove('blue'));  //Удаление класса
-
-// console.log(btns[0].classList.toggle('red'));   //тогл(добавление/удаление если нет/есть) класса
-
-//contains - возвращает true/false если у обьекта есть/нет класса
-
-// console.log(btns[1].classList.toggle('red'));
-// if (btns[1].classList.contains('red')) {
-//     console.log('red');
-// }
-
-// btns[0].addEventListener('click', () => {
-//     if (!btns[1].classList.contains('red')) {
-//         btns[1].classList.add('red');
-//     } else {
-//         btns[1].classList.remove('red');
-//     }
-// });
-
-btns[0].addEventListener('click', () => {
-        btns[1].classList.toggle('red');
-});
-
-// console.log(btns(0).className);
-
-//делигирование, для размножение событий для всех элементов
-
-wrapper.addEventListener('click', (event) => {
-    // console.dir(event.target);
-    
-    // if (event.target && event.target.tagName == "BUTTON") {
-    //     console.log('Hello!');        
-    // }
-
-    // if (event.target && event.target.classList.contains('blue')) {
-    //     console.log('Hello!');        
-    // }
-
-    //гуглы любят
-
-    if (event.target && event.target.matches('button.red')) {
-        console.log('Hello!');        
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
     }
 
+    function showTabContent(i = 0) {    //подстановка значения по умолчанию для функции без паарметров
+    //    tabsContent[i].style.display = 'block';
+       tabsContent[i].classList.add('show', 'fade');    //переделываем через классы
+       tabsContent[i].classList.remove('hide');    //переделываем через классы
+       tabs[i].classList.add('tabheader__item_active'); 
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 });
-
-// btns.forEach(btn => {   //аналог при помощи перебора. не добавляет к новым кнопкам созданным пожже
-//     btn.addEventListener('click', () => {
-//         console.log('Hewllo!');
-//     });
-// }); 
-
-
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
-
-
-
