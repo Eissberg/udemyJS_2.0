e1d917a51b7b6ca4c48ff6007f591df1372bdc23
Eissberg <jeka.chk@gmail.com>
2021-03-11 17:21:28 +0200

    052 AJAX и общение с сервером

---
+++ 053 Реализация скрипта отправки данных на сервер/js/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const inputHrn = document.querySelector('#hrn'),
+      inputUsd = document.querySelector('#usd');
+
+inputHrn.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    // request.open(method, url, async, login, pass);   Состав запроса
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    // request.addEventListener('readystatechange', () => {         // через readystatechange
+    //     if (request.readyState === 4 && request.status === 200) {
+    //         console.log(request.response);
+    //         const data = JSON.parse(request.response);
+    //         inputUsd.value = (+inputHrn.value / data.current.usd).toFixed(2);
+    //     } else {
+    //         inputUsd.value = "Something goes wrong...";
+    //     }
+    // });
+
+    request.addEventListener('load', () => {         // через load
+            if (request.status === 200) {
+                const data = JSON.parse(request.response);
+                inputUsd.value = (+inputHrn.value / data.current.usd).toFixed(2);
+            } else {
+                inputUsd.value = "Something goes wrong...";
+            }
+        });
+
+
+
+    //СВОЙСТВА ЗАПРОСА
+
+    // status
+    // statusText
+    // response
+    // responseText
+    // readyState
+
+    // СОБЫТИЯ ОБЬЕКТА
+
+    //readystatechange\
+    //load
+});
